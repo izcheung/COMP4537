9d99502b9fd7ce470c284d65cba7f1a65bdd12a5
izcheung <icheung24@my.bcit.ca>
2025-09-10 12:31:31 -0700

    feat: add logic for reader page

---
+++ labs/1/js/reader.js
@@ -0,0 +1,66 @@
+import { Navigation } from "./common.js";
+import { MESSAGES } from "../lang/messages/en/user.js";
+
+class ReaderTextAreaManager {
+  constructor() {
+    this.container = document.querySelector("section");
+  }
+
+  addElement(text) {
+    const wrapper = document.createElement("div");
+    const newTextInput = document.createElement("textarea");
+    newTextInput.value = text;
+    wrapper.appendChild(newTextInput);
+    this.container.appendChild(wrapper);
+  }
+
+  addTimeStamp(time) {
+    const timeStamp = document.querySelector("#timestamp");
+    timeStamp.textContent = `${MESSAGES.readTimeStamp} ${time}`;
+  }
+}
+
+class ReadLocalStorage {
+  constructor(textAreaManager) {
+    this.intervalId = null;
+    this.readerTextAreaManager = textAreaManager;
+    this.existingNotes = new Set();
+  }
+
+  start(interval = 2000) {
+    this.intervalId = setInterval(() => this.readAll(), interval);
+  }
+
+  readAll() {
+    const notesJSON = localStorage.getItem("notes");
+    const notes = JSON.parse(notesJSON || "[]");
+    notes.forEach((note) => {
+      if (!this.existingNotes.has(note.id)) {
+        this.readerTextAreaManager.addElement(note.content);
+        this.existingNotes.add(note.id);
+      }
+    });
+    const timeNow = new Date().toLocaleTimeString("en-US", {
+      hour12: true,
+    });
+    this.readerTextAreaManager.addTimeStamp(timeNow);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+  }
+}
+
+class ReaderApp {
+  constructor() {
+    const readerTextAreaManager = new ReaderTextAreaManager();
+    this.readLocalStorage = new ReadLocalStorage(readerTextAreaManager);
+    this.backButton = new Navigation();
+  }
+
+  run() {
+    this.readLocalStorage.start();
+  }
+}
+
+new ReaderApp().run();
